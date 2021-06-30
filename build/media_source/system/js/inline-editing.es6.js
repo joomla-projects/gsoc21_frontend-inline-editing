192c925100fcf3f90fe9f22fcb6a8ca0567dcd14
Rahul Saini <rs4231199@gmail.com>
2021-06-30 11:46:58 +0530

    Added: Cancel button

--- build/media_source/system/js/inline-editing.es6.js
+++ build/media_source/system/js/inline-editing.es6.js
@@ -2,7 +2,7 @@
   'use strict';
 
   // TODO
-  // Shift some code to css file
+  // Shift some code to the css file
 
   // Get text area and loading icon
   const GetTextAreaWrapper = (oldContent) => {
@@ -12,7 +12,10 @@
     const loader = document.createElement('img');
     const wrap = document.createElement('div');
 
-    if (!icon || !loader || !textArea || !wrap) {
+    const cancelButton = document.createElement('button');
+    const cancelButtonIcon = document.createElement('span');
+
+    if (!icon || !loader || !textArea || !wrap || !cancelButton || !cancelButtonIcon) {
       return null;
     }
 
@@ -20,6 +23,17 @@
     textArea.value = oldContent;
     textArea.rows = 1;
     textArea.style.width = 'auto';
+
+    // Cancel button
+    cancelButtonIcon.classList.add('icon-times');
+    cancelButtonIcon.setAttribute('aria-hidden', 'true');
+    cancelButton.type = 'button';
+    cancelButton.classList.add('btn-danger');
+    Object.assign(cancelButton.style, {
+      float: 'left',
+      margin: '2px',
+    });
+    cancelButton.appendChild(cancelButtonIcon);
 
     // Loading icon
     loader.src = icon;
@@ -36,13 +50,18 @@
     loader.classList.add('d-none');
 
     // Wrap textArea and loader inside a div
-    wrap.classList.add('d-none');
     wrap.style.position = 'relative';
     wrap.style.display = 'inline-block';
+    wrap.appendChild(cancelButton);
     wrap.appendChild(textArea);
     wrap.appendChild(loader);
 
-    return { wrap, textArea, loader };
+    return {
+      wrap,
+      textArea,
+      loader,
+      cancelButton,
+    };
   };
 
   const Error = (element) => {
@@ -60,59 +79,71 @@
     if (!wrapAndTextArea) {
       return;
     }
-    const { wrap, textArea, loader } = wrapAndTextArea;
+    const {
+      wrap,
+      textArea,
+      loader,
+      cancelButton,
+    } = wrapAndTextArea;
 
     // Add wrap to the DOM
     _this.parentNode.insertBefore(wrap, _this.nextSibling);
 
     // Hide custom field value and show a text area
     _this.classList.add('d-none');
-    wrap.classList.remove('d-none');
     textArea.focus();
+
+    let cancel = false;
+    // Restore original element on Cancel
+    cancelButton.addEventListener('click', () => {
+      cancel = true;
+      wrap.remove();
+      _this.classList.remove('d-none');
+    });
 
     // Send Ajax request and update front-end when user focuses out of textArea
     textArea.addEventListener('focusout', () => {
-      // replace focus-out with something more suitable
-      //    maybe focus-out and windows.still_focused
-      // console.log(document.hidden)
-      // if(!document.hasFocus())
-      // {
-      //   console.log('fsadf')
-      //   return;
-      // }
+      const AjaxCall = () => {
+        const newValue = textArea.value;
+        const url = '?option=com_fields&task=Field.saveField&format=json';
+        const data = `field_id=${fieldId}&item_id=${itemId}&value=${newValue}&${Joomla.getOptions('csrf.token', '')}=1`;
 
-      const newValue = textArea.value;
-      const url = '?option=com_fields&task=Field.saveField&format=json';
-      const data = `field_id=${fieldId}&item_id=${itemId}&value=${newValue}&${Joomla.getOptions('csrf.token', '')}=1`;
+        textArea.disabled = true;
+        loader.classList.remove('d-none');
 
-      textArea.disabled = true;
-      loader.classList.remove('d-none');
+        const AjaxEnd = () => {
+          wrap.remove();
+          _this.classList.remove('d-none');
+        };
 
-      const AjaxEnd = () => {
-        wrap.remove();
-        _this.classList.remove('d-none');
+        Joomla.request({
+          url,
+          method: 'POST',
+          data,
+          onSuccess: (response) => {
+            const responseJson = JSON.parse(response);
+
+            if (responseJson.data.saved === true) {
+              _this.innerHTML = newValue;
+            } else {
+              Error(_this);
+            }
+
+            AjaxEnd();
+          },
+          onError: () => {
+            Error(_this);
+            AjaxEnd();
+          },
+        });
       };
 
-      Joomla.request({
-        url,
-        method: 'POST',
-        data,
-        onSuccess: (response) => {
-          const responseJson = JSON.parse(response);
-
-          if (responseJson.data.saved === true) {
-            _this.innerHTML = newValue;
-          } else {
-            Error(_this);
-          }
-
-          AjaxEnd();
-        },
-        onError: () => {
-          Error(_this);
-          AjaxEnd();
-        },
-      });
+      setTimeout(() => {
+        if (!document.hasFocus() || cancel) {
+          return;
+        }
+        AjaxCall();
+      }, 200);
     });
   };
 
