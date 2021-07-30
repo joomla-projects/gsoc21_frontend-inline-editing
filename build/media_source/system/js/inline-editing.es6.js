496b7268b57a6b14f6fcfd6f71943984ed811a97
Rahul Saini <rs4231199@gmail.com>
2021-07-30 23:16:08 +0530

    (+) Save data using Ajax request.

--- build/media_source/system/js/inline-editing.es6.js
+++ build/media_source/system/js/inline-editing.es6.js
@@ -1,15 +1,100 @@
 (() => {
   'use strict';
 
-  // const addForm = (element, field) => {
-  // const dimensions = element.getBoundingClientRect();
-  // console.log(element, field);
-  // [TODO] create and add a form
-  // makeForm();
-  // append it to the body and give it an absolute position
-  // };
-
   const options = Joomla.getOptions('inline-editing');
+
+  document.body.innerHTML += '<form action="" class="inline-editing-container"></form>';
+  const container = document.querySelector('.inline-editing-container');
+  let selectedElement = null;
+
+  container.addEventListener('click', (e) => e.stopPropagation());
+
+  const resetContainer = () => {
+    container.style = null;
+    container.innerHTML = null;
+    container.classList.add('d-none');
+  };
+  resetContainer();
+
+  container.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (selectedElement === null) {
+      return;
+    }
+
+    const element = selectedElement;
+    const key = element.classList[1];
+    const data = options[key];
+
+    // Prepare the form data
+    const formData = new FormData(container);
+    formData.append('task', `${data.controller}.saveInline`);
+    formData.append(Joomla.getOptions('csrf.token'), '1');
+
+    // Get the updated value
+    const fieldName = `[${data.fieldName}]`;
+    const fieldGroup = ((data.fieldGroup != null) ? `[${data.fieldGroup}]` : '');
+
+    // [TODO] Get the right updated value.
+    const updatedValue = formData.get(`jform${fieldGroup}${fieldName}`);
+    // console.log(updatedValue);
+
+    // for (var pair of formData.entries()) {
+    //   console.log(pair[0] + ', ' + pair[1]);
+    // }
+
+    // Make the ajax call
+    Joomla.request({
+      url: data.url,
+      method: 'POST',
+      data: formData,
+      onSuccess: (resp) => {
+        let response;
+
+        try {
+          response = JSON.parse(resp);
+        } catch (e) {
+          Joomla.renderMessages({ error: ['Invalid response.'] });
+          return;
+        }
+
+        if (response.message) {
+          Joomla.renderMessages({ error: [response.message] });
+        }
+        if (response.messages) {
+          Joomla.renderMessages(response.messages);
+        }
+        if (response.success === true) {
+          element.innerHTML = updatedValue;
+        }
+      },
+      onError: () => {
+        Joomla.renderMessages({ error: ['Something went wrong!'] });
+      },
+    });
+  });
+
+  document.addEventListener('click', () => {
+    container.requestSubmit();
+    selectedElement = null;
+    resetContainer();
+  });
+
+  const addToContainer = (element, field) => {
+    // Cover element with the container.
+    const cover = () => {
+      container.style.left = `${element.offsetLeft}px`;
+      container.style.top = `${element.offsetTop}px`;
+      container.style.minHeight = `${element.offsetHeight}px`;
+      container.style.minWidth = `${element.offsetWidth}px`;
+    };
+    cover();
+    window.addEventListener('resize', cover);
+
+    // Add field to the container.
+    container.innerHTML = field;
+    container.classList.remove('d-none');
+  };
 
   const addInputField = (element) => {
     const key = element.classList[1];
@@ -47,7 +132,12 @@
             Joomla.renderMessages(response.messages);
           }
         } else {
-          // addForm(element, response.data.html);
+          // 1. Add to the DOM.
+          selectedElement = element;
+          addToContainer(element, response.data.html);
+          // 2. Render field properly / Load scripts and styles
+
+          // 3. addForm(element, response.data.html);
           // console.log(response.data.html);
         }
       },
